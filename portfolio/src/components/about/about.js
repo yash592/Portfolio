8b0f848cdee814fa6252bbd4b864b2407d93a762
Yash Rajgor <yash592@gmail.com>
2018-02-07 14:29:22 -0800

    social

--- portfolio/src/components/about/about.js
+++ portfolio/src/components/about/about.js
@@ -7,6 +7,7 @@ const About = () => (
   <div className = "about">
         <h6>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content
             here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text</h6>
+            <div class="social-icons"><a href="#"><i class="icon ion-social-github-outline"></i></a><a href="#"><i class="icon ion-social-linkedin-outline"></i></a><a href="#"><i class="icon ion-ios-email-outline"></i></a><a href="#"><i class="icon ion-social-facebook-outline"></i></a></div>
     </div>
 )
 
