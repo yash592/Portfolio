93f5f4a567d771898c9d06d66d41c3ea960d6e67
Yash Rajgor <yash592@gmail.com>
2018-02-11 00:13:23 -0800

    fixed logos

--- portfolio/src/components/about/about.js
+++ portfolio/src/components/about/about.js
@@ -5,6 +5,8 @@ import "./about.css";
 
 const About = () => (
   <div className = "about" id="abt">
+        <h4>Yash Rajgor</h4>
+        <h4>Full Stack Developer, Engineer</h4>
         <h6>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content
             here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text</h6>
             <div class="social-icons">
