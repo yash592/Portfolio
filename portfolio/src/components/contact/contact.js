e4e95507134ab4ace9bc912ce5daeaaf48fc6585
Yash Rajgor <yash592@gmail.com>
2018-02-11 12:42:35 -0800

    Color fixes

--- portfolio/src/components/contact/contact.js
+++ portfolio/src/components/contact/contact.js
@@ -7,9 +7,9 @@ const Contact = () => (
   <h1>Get in touch</h1>
   <h4>I would love to hear from you so please get in touch</h4>
     <div class="icons">
-      <a href="https://github.com/yash592"><img className="social" src = "https://cdn3.iconfinder.com/data/icons/free-social-icons/67/github_circle_gray-128.png" /></a>
-      <a href="https://www.linkedin.com/in/yashrajgor/"><img className="social" src = "https://cdn3.iconfinder.com/data/icons/free-social-icons/67/linkedin_circle_gray-128.png" /></a>
-      <a href="https://www.facebook.com/YashRajgor"><img className="social" src = "https://cdn3.iconfinder.com/data/icons/free-social-icons/67/facebook_circle_gray-128.png" /></a>
+      <a href="https://github.com/yash592" target="_blank"><img className="social" src = "https://cdn3.iconfinder.com/data/icons/free-social-icons/67/github_circle_gray-128.png" /></a>
+      <a href="https://www.linkedin.com/in/yashrajgor/" target="_blank"><img className="social" src = "https://cdn3.iconfinder.com/data/icons/free-social-icons/67/linkedin_circle_gray-128.png" /></a>
+      <a href="https://www.facebook.com/YashRajgor" target="_blank"><img className="social" src = "https://cdn3.iconfinder.com/data/icons/free-social-icons/67/facebook_circle_gray-128.png" /></a>
     </div>
 
   </div>
